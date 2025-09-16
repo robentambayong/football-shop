e6edfe54b435c4e6d3e9568b9aff6f629adc347e
robentambayong <tambayongroben@gmail.com>
2025-09-16 21:47:17 +0700

    Final Detail

--- football_shop/settings.py
+++ football_shop/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "django.contrib.humanize",
     'main',
 ]
 
@@ -60,7 +61,7 @@ ROOT_URLCONF = 'football_shop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,25 @@
 from django.urls import path
-from . import views
+from main.views import (
+    show_main,
+    create_product,
+    show_product,
+    show_xml,
+    show_json,
+    show_xml_by_id,
+    show_json_by_id,
+)
+
+app_name = "main"
 
 urlpatterns = [
-    path("", views.home, name="home"),
+    # Web pages
+    path("", show_main, name="show_main"),
+    path("create-product/", create_product, name="create_product"),
+    path("product/<int:id>/", show_product, name="show_product"),
+
+    # Data delivery (Assignment 3 requirements)
+    path("xml/", show_xml, name="show_xml"),
+    path("json/", show_json, name="show_json"),
+    path("xml/<int:id>/", show_xml_by_id, name="show_xml_by_id"),
+    path("json/<int:id>/", show_json_by_id, name="show_json_by_id"),
 ]
